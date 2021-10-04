a4bfc2db31b8126504ae5f55c959be2bf6a071c9
sunquan <1585437938@qq.com>
2021-10-04 10:36:12 +0800

    新增vue.config.js文件，配置favicon图标

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  baseUrl:'./',
+  assertsDir:'static',
+  pwa:{
+    iconPath:{
+      favicon32:'./favicon.ico',
+      favicon16:'./favicon.ico',
+      appleTouchIcon:'./favicon.ico',
+      maskIcon:'./favicon.ico',
+      msTilelmage:'./favicon.ico',
+    }
+  },
+}
+
