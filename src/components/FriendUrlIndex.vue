af1ee1e27bd2e0da7e41fbf8511ff5ba2e36f6f6
sunquan <1585437938@qq.com>
2021-11-15 20:51:07 +0800

    1、友链设置过长隐藏；

--- src/components/FriendUrlIndex.vue
+++ src/components/FriendUrlIndex.vue
@@ -32,7 +32,7 @@
         <el-table :data="FriendUrlList" style="width: 100%">
           <el-table-column prop="FriendUrlNickName" label="友链名称"></el-table-column>
           <el-table-column prop="FriendUrlAdress" label="友链地址"></el-table-column>
-          <el-table-column prop="FriendUrlIcoUrl" label="图标地址"></el-table-column>
+          <el-table-column prop="FriendUrlIcoUrl" label="图标地址" width="300" :show-overflow-tooltip="true"></el-table-column>
           <el-table-column prop="FriendUrlDescript" label="个人描述"></el-table-column>
           <el-table-column prop="order" label="优先级（从0开始）"></el-table-column>
           <el-table-column fixed="right" label="操作" width="130">
@@ -59,135 +59,135 @@
 </template>
 
 <script>
-  export default {
-    name: "TagIndex",
-    data: function () {
-      return {
-        FriendUrlList: [],
-        dialogFormVisible: false,
-        form: {
-          FriendUrlNickName:'',
-          FriendUrlAdress:'',
-          FriendUrlIcoUrl: '',
-          FriendUrlDescript: ''
-        },
-        formLabelWidth: '120px',
+export default {
+  name: "TagIndex",
+  data: function () {
+    return {
+      FriendUrlList: [],
+      dialogFormVisible: false,
+      form: {
+        FriendUrlNickName: '',
+        FriendUrlAdress: '',
+        FriendUrlIcoUrl: '',
+        FriendUrlDescript: ''
+      },
+      formLabelWidth: '120px',
 
-        FriendUrlTotal: 0,
-        PagiSize: 10,
-        MyCurPage: 1
-      }
+      FriendUrlTotal: 0,
+      PagiSize: 10,
+      MyCurPage: 1
+    }
+  },
+  methods: {
+    /*监听新增弹框*/
+    OnOpenDialog: function () {
+      // 清空表单数据
+      this.form = {};
+      this.dialogFormVisible = true;
     },
-    methods: {
-      /*监听新增弹框*/
-      OnOpenDialog:function(){
-        // 清空表单数据
-        this.form = {};
-        this.dialogFormVisible = true;
-      },
-      /*监听弹框提交*/
-      OnDialogSubmit:function(){
-        var That = this;
-        if(this.form.FriendUrlDescript){
-          this.SQAjax({
-            Url:'/api/FriendUrlEditor/backend',
-            RequestData:That.form,
-            Success:function (data) {
-              if(That.form._id){
-                delete That.form._id;
-              }
-              That.SkipTo(That.MyCurPage);
+    /*监听弹框提交*/
+    OnDialogSubmit: function () {
+      var That = this;
+      if (this.form.FriendUrlDescript) {
+        this.SQAjax({
+          Url: '/api/FriendUrlEditor/backend',
+          RequestData: That.form,
+          Success: function (data) {
+            if (That.form._id) {
+              delete That.form._id;
             }
-          });
-
-          this.dialogFormVisible = false;
-        }
-      },
-      OnDialogCancel:function(){
-        if(this.form._id){
-          delete this.form._id;
-        }
-        this.dialogFormVisible = false;
-      },
-      /*渲染标签列表*/
-      GetData: function () {
-        var That = this;
-        That.SQAjax({
-          Url:'/api/FriendUrlRead/backend',
-          RequestData: {
-            PagnationData: {
-              Skip:0,
-              Limit:11
-            }
-          },
-          Success:function (data) {
-            if (data.length > 10) {
-              data.pop();
-
-              That.SQAjax({
-                Url: '/api/getfriendurlnum',
-                Success: function (data) {
-                  That.FriendUrlTotal = data;
-                }
-              });
-            }
-
-            That.FriendUrlList = data;
-          }
-        });
-      },
-      /*删除标签*/
-      DeleteFriendUrl:function (Id) {
-        var That = this;
-
-        That.SQAjax({
-          Url:'/api/FriendUrlDelete/backend',
-          RequestData:{
-            _id:Id
-          },
-          Success:function (data) {
-            That.$message('删除成功');
             That.SkipTo(That.MyCurPage);
           }
         });
-      },
-      /*编辑标签*/
-      EditFriendUrl:function (RowData) {
-        this.OnOpenDialog();
-        this.form = RowData;
-      },
-      // 翻页方法
-      ChangeCurPage:function(CurPage){
-        this.SkipTo(CurPage);
-        this.MyCurPage = CurPage;
-      },
-      NextPage:function (CurPage) {
-        this.SkipTo(CurPage);
-        this.MyCurPage = CurPage;
-      },
-      SkipTo:function (CurPage) {
-        var That = this;
-        That.SQAjax({
-          Url:'/api/FriendUrlRead/backend',
-          RequestData: {
-            PagnationData: {
-              Skip:(CurPage-1) * 10,
-              Limit:10
-            }
-          },
-          Success:function (data) {
-            That.FriendUrlList = data;
-          }
-        });
+
+        this.dialogFormVisible = false;
       }
     },
-    mounted: function () {
-      this.GetData();
-      this.bus.$emit('Topbar',{
-        MenuHighLight:'5'
+    OnDialogCancel: function () {
+      if (this.form._id) {
+        delete this.form._id;
+      }
+      this.dialogFormVisible = false;
+    },
+    /*渲染标签列表*/
+    GetData: function () {
+      var That = this;
+      That.SQAjax({
+        Url: '/api/FriendUrlRead/backend',
+        RequestData: {
+          PagnationData: {
+            Skip: 0,
+            Limit: 11
+          }
+        },
+        Success: function (data) {
+          if (data.length > 10) {
+            data.pop();
+
+            That.SQAjax({
+              Url: '/api/getfriendurlnum',
+              Success: function (data) {
+                That.FriendUrlTotal = data;
+              }
+            });
+          }
+
+          That.FriendUrlList = data;
+        }
+      });
+    },
+    /*删除标签*/
+    DeleteFriendUrl: function (Id) {
+      var That = this;
+
+      That.SQAjax({
+        Url: '/api/FriendUrlDelete/backend',
+        RequestData: {
+          _id: Id
+        },
+        Success: function (data) {
+          That.$message('删除成功');
+          That.SkipTo(That.MyCurPage);
+        }
+      });
+    },
+    /*编辑标签*/
+    EditFriendUrl: function (RowData) {
+      this.OnOpenDialog();
+      this.form = RowData;
+    },
+    // 翻页方法
+    ChangeCurPage: function (CurPage) {
+      this.SkipTo(CurPage);
+      this.MyCurPage = CurPage;
+    },
+    NextPage: function (CurPage) {
+      this.SkipTo(CurPage);
+      this.MyCurPage = CurPage;
+    },
+    SkipTo: function (CurPage) {
+      var That = this;
+      That.SQAjax({
+        Url: '/api/FriendUrlRead/backend',
+        RequestData: {
+          PagnationData: {
+            Skip: (CurPage - 1) * 10,
+            Limit: 10
+          }
+        },
+        Success: function (data) {
+          That.FriendUrlList = data;
+        }
       });
     }
+  },
+  mounted: function () {
+    this.GetData();
+    this.bus.$emit('Topbar', {
+      MenuHighLight: '5'
+    });
   }
+}
 </script>
 
 <style scoped>
