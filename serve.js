b68d4e68e495432bb6de6e6e7b4c3f273938d886
sunquan <1585437938@qq.com>
2021-12-19 10:09:04 +0800

    修改服务端代码

---
+++ serve.js
@@ -0,0 +1,33 @@
+//引入express中间件
+var express = require('express');
+var compression = require('compression');
+let axios = require('axios');
+const http = require('http');
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(compression());
+app.use(bodyParser.json());
+
+http.createServer(app).listen(68);
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname});
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(req.params[0], {root: __dirname});
+});
+
+app.post('/*', (req, res) => {
+  let pathName = req.params[0],
+    paramType = pathName.split('/')[0];
+  axios.post(
+    'http://39.104.22.73:8888' + pathName.replace('api',''),
+    req.body
+  ).then(function (response) {
+    res.end(JSON.stringify(response.data));
+  }).catch(function (e) {
+    console.log('ajax error');
+  });
+});
