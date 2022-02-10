8bd9422ee285f5f7ea4f1b72b2bf1b630ac03226
sunquan <1585437938@qq.com>
2022-02-10 23:41:30 +0800

    1、优化个人中心样式；2、修改压缩后产出文件夹的名称；

--- config/index.js
+++ config/index.js
@@ -53,10 +53,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../blog-admin/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../blog-admin'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
