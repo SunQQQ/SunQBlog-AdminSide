ee1127327c27b4c2adbaa98eeca0ba2974424ce1
sunquan <1585437938@qq.com>
2022-07-17 21:52:02 +0800

    添加对评论的修改功能

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -4,23 +4,28 @@
 <template>
   <div>
     <div class="RightContent">
-      <!-- <el-button type="primary" >批量删除</el-button> -->
-      <el-dialog title="管理友链" :visible.sync="dialogFormVisible">
+      <div class="ArticleList">
+        <!-- <el-button type="primary" >批量删除</el-button> -->
+        <el-dialog title="修改评论" :visible.sync="dialogEdit">
         <el-form :model="form">
-          <el-form-item label="评论时间" :label-width="formLabelWidth">
-            <el-input v-model="form.ArticleCommentDate"></el-input>
-          </el-form-item>
           <el-form-item label="评论人" :label-width="formLabelWidth">
             <el-input v-model="form.ArticleCommentNickName"></el-input>
           </el-form-item>
           <el-form-item label="评论内容" :label-width="formLabelWidth">
             <el-input v-model="form.ArticleCommentText"></el-input>
           </el-form-item>
+          <el-form-item label="评论来源" :label-width="formLabelWidth">
+            <el-input v-model="form.LocationCityName"></el-input>
+          </el-form-item>
         </el-form>
-      </el-dialog>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="OnDialogCancel()">取 消</el-button>
+            <el-button type="primary" @click="OnDialogSubmit()">确 定</el-button>
+          </div>
+        </el-dialog>
 
-      <!--表格操作栏-->
-      <el-table :data="CommentList" style="width: 100%" border :header-cell-style="{background:'#f7f7f7'}">
+        <!--表格操作栏-->
+        <el-table :data="CommentList" style="width: 100%" border :header-cell-style="{background:'#f7f7f7'}">
         <el-table-column prop="ArticleName" label="评论文章"></el-table-column>
         <el-table-column prop="ArticleCommentNickName" label="评论人"></el-table-column>
         <el-table-column label="评论内容">
@@ -28,24 +33,28 @@
             <div v-html="scope.row.ArticleCommentText"></div>
           </template>
         </el-table-column>
+        <el-table-column prop="LocationCityName" label="评论来源"></el-table-column>
         <el-table-column prop="ArticleCommentDate" label="评论时间"></el-table-column>
         <el-table-column fixed="right" label="操作" width="130">
           <template slot-scope="scope">
+            <el-button @click="edit(scope.row)" type="text" size="small" class="warning-color">修改</el-button>
             <el-button @click="DeleteComment(scope.row._id,scope.row.ArticleId)" type="text" size="small" class="danger-color">删除</el-button>
           </template>
         </el-table-column>
-      </el-table>
+        </el-table>
 
-      <!--分页器-->
-      <div v-if="CommentTotal>10">
-        <el-pagination layout="prev, pager, next,total"
+        <!--分页器-->
+        <div v-if="CommentTotal>10">
+          <el-pagination layout="prev, pager, next,total"
                        :total=CommentTotal
                        :page-size=PageSize
                        @current-change="ChangeCurPage"
                        @next-click="NextPage"
                        @prev-click="NextPage"
         >
-        </el-pagination>
+          </el-pagination>
+        </div>
+
       </div>
     </div>
   </div>
@@ -57,12 +66,13 @@
     data:function(){
       return {
         CommentList: [],
-        dialogFormVisible: false,
+        dialogEdit: false,
         form: {
-          ArticleCommentDate:'',
+          LocationCityName:'',
           ArticleCommentNickName:'',
           ArticleCommentText:''
         },
+        formLabelWidth: '120px',
         CommentTotal: 0,
         PageSize: 10,
         
@@ -122,6 +132,37 @@
           }
         });
       },
+      OnDialogCancel:function(){
+        if(this.form.TagId){
+          delete this.form.TagId;
+        }
+        this.dialogEdit = false;
+      },
+      /*监听弹框提交*/
+      OnDialogSubmit:function(){
+        var That = this;
+        if(this.form.ArticleCommentNickName && this.form.ArticleCommentText){
+          this.SQAjax({
+            Url:'/api/ArticleCommentUpdate/backend',
+            RequestData:this.form,
+            Success:function () {
+              That.SkipTo(That.MyCurPage);
+              That.dialogEdit = false;
+            }
+          });
+        }
+      },
+      /**
+      * 修改评论
+      */
+      edit:function (Row) {
+        this.form.ArticleCommentNickName = Row.ArticleCommentNickName;
+        this.form.ArticleCommentText = Row.ArticleCommentText;
+        this.form.LocationCityName = Row.LocationCityName;
+        this.form._id = Row._id;
+        this.dialogEdit = true;
+      },
+
       // 传入文章id，给对应文章评论数减1
       UpdateArticleCommentNum: function (id,type) {
         this.SQAjax({
@@ -130,9 +171,7 @@
             _id: id,
             type: type
           },
-          Success: function (data) {
-
-          }
+          Success: function (data) {}
         });
       },
       // 翻页方法
@@ -155,6 +194,10 @@
             }
           },
           Success:function (data) {
+            data.forEach(element => {
+              element.ArticleCommentDate = new Date(element.ArticleCommentDate).toLocaleString();
+            });   
+
             That.CommentList = data;
           }
         });
