192662a5dacad987b307925031a7e1c687198eb9
sunquan <1585437938@qq.com>
2023-09-17 22:26:05 +0800

    优化代码，input监听enter相关逻辑

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -3,11 +3,11 @@
     <el-form ref="form" :model="form" label-width="60px">
       <div style="text-align: center"><h2>sunq's blog 管理后台</h2></div>
       <div style="text-align: center;color:#409EFF"><h4>（后台对外开发，请直接登录）</h4></div>
-      <el-form-item label="账号">
-        <el-input v-model="form.CnName"></el-input>
+     <el-form-item label="账号">
+        <el-input v-model="form.CnName" @keydown.enter.native="OnLogin()"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="form.PassWord" type="password"></el-input>
+     <el-form-item label="密码">
+        <el-input v-model="form.PassWord" type="password" show-password=true @keydown.enter.native="OnLogin()"></el-input>
       </el-form-item>
       <div style="text-align: center">
         <el-button type="primary" @click="OnLogin()">登录</el-button>
@@ -88,22 +88,7 @@ export default {
         }
       }).catch(function (error) {
       });
-    },
-    // 监听enter键
-    enterKey: function(){
-      let that = this;
-      const code = event.keyCode ? event.keyCode : (event.which ? event.which : event.charCode);
-      
-      if(code == 13) that.OnLogin();
     }
-  },
-  // 生命周期不能使用箭头函数
-  mounted: function(){
-    document.addEventListener('keyup',this.enterKey);
-  },
-  // 本组件销毁时注意移除监听，否则其他页面点击enter也会登陆
-  destroyed: function(){
-    document.removeEventListener('keyup', this.enterKey);
   }
 }
 </script>
