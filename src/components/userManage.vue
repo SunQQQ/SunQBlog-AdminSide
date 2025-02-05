d16c611925877b94c2a167dadbb9081b8ac6aa02
sunquan <1585437938@qq.com>
2025-02-05 17:51:49 +0800

    改造修改用户

--- src/components/userManage.vue
+++ src/components/userManage.vue
@@ -5,8 +5,8 @@
         <div style="margin-bottom:10px" v-show="userRole == 'master'">
           <el-button type="primary" @click="OpenCreateDialog()" plain>创建账号</el-button>
         </div>
-        
-        <el-dialog :title= "dailogTitle" :visible.sync="dialogFormVisible">
+
+        <el-dialog :title="dailogTitle" :visible.sync="dialogFormVisible">
           <el-form :model="form">
             <el-form-item label="账号名称" :label-width="formLabelWidth">
               <el-input v-model="form.username"></el-input>
@@ -15,7 +15,7 @@
               <el-input v-model="form.password"></el-input>
             </el-form-item>
             <el-form-item label="角色" :label-width="formLabelWidth">
-              <el-select v-model="form.role" placeholder="请选择" :disabled="userRole=='user'" style="width: 100%;">
+              <el-select v-model="form.role" placeholder="请选择" :disabled="userRole == 'user'" style="width: 100%;">
                 <el-option label="普通用户" value="user"></el-option>
                 <el-option label="管理员" value="master"></el-option>
               </el-select>
@@ -31,7 +31,7 @@
         </el-dialog>
 
         <!--表格操作栏-->
-        <el-table :data="userList" style="width: 100%" border :header-cell-style="{background:'#f7f7f7'}">
+        <el-table :data="userList" style="width: 100%" border :header-cell-style="{ background: '#f7f7f7' }">
           <el-table-column prop="name" label="账号名称"></el-table-column>
           <el-table-column prop="password" label="密码"></el-table-column>
           <el-table-column prop="role" label="角色"></el-table-column>
@@ -39,13 +39,12 @@
           <el-table-column prop="email" label="邮箱"></el-table-column>
           <el-table-column fixed="right" label="操作" width="130">
             <template slot-scope="scope">
-              <el-button @click="UpdateHeartfelt(scope.row._id,scope.row.name,scope.row.password,scope.row.role,scope.row.email)" type="text" size="small" class="warning-color">
+              <el-button
+                @click="UpdateHeartfelt(scope.row.id, scope.row.name, scope.row.password, scope.row.role, scope.row.email)"
+                type="text" size="small" class="warning-color">
                 编辑
               </el-button>
-              <el-button 
-                @click="DeleteHeartfelt(scope.row.name)" 
-                v-if="userRole == 'master'"
-                type="text" size="small" 
+              <el-button @click="DeleteHeartfelt(scope.row.name)" v-if="userRole == 'master'" type="text" size="small"
                 class="danger-color">
                 删除
               </el-button>
@@ -58,100 +57,111 @@
 </template>
 
 <script>
-  export default {
-    name: "userManage",
-    data:function () {
-      return {
-        userList:'',
-        dialogFormVisible:false,
-        formLabelWidth: '80px',
-        form:{
-          username:'',
-          password: '',
-          role:'',
-          email: ''
-        },
-        userRole: JSON.parse(window.localStorage.getItem("sqBlogUserInfo")).role,
-        dailogTitle:'新增账号' // 弹框标题
-      }
+export default {
+  name: "userManage",
+  data: function () {
+    return {
+      userList: '',
+      dialogFormVisible: false,
+      formLabelWidth: '80px',
+      form: {
+        id:"",
+        username: '',
+        password: '',
+        role: '',
+        email: ''
+      },
+      userRole: JSON.parse(window.localStorage.getItem("sqBlogUserInfo")).role,
+      dailogTitle: '新增账号' // 弹框标题
+    }
+  },
+  methods: {
+    // 打开新增弹框
+    OpenCreateDialog: function () {
+      this.form.id = '';
+      this.form.username = '';
+      this.form.password = password;
+      this.form.role = '';
+      this.form.email = '';
+
+      this.dialogFormVisible = true;
     },
-    methods:{
-      // 打开新增弹框
-      OpenCreateDialog:function(){
-        this.form._id = '';
-        this.form.username = '';
-        this.form.role = '';
-        this.form.email = '';
+    //修改账号
+    UpdateHeartfelt: function (Id, username, password, role, email) {
+      this.form.id = Id;
+      this.form.username = username;
+      this.form.password = password;
+      this.form.role = role;
+      this.form.email = email;
 
-        this.dialogFormVisible = true;
-      },
-      // 关闭新增弹框
-      CloseCreateDialog:function(){
-        this.dialogFormVisible = false;
-      },
-      // 提交数据
-      PostHeartfelt:function(){
-        var That = this;
+      this.dialogFormVisible = true;
+      this.dailogTitle = '编辑账号';
+    },
+    // 关闭新增弹框
+    CloseCreateDialog: function () {
+      this.dialogFormVisible = false;
+    },
+    // 提交数据
+    PostHeartfelt: function () {
+      var That = this;
 
-        if(this.form.username && this.form.password && this.form.role){
-          this.SQAjax({
-            Url:'/api/register',
-            RequestData:this.form,
-            Success:function () {
-              That.GetHeartfeltList();
-              That.CloseCreateDialog();
-            }
-          });
-        }
-      },
-      //修改账号
-      UpdateHeartfelt:function(Id,username,password,role,email){
-        this.form._id = Id;
-        this.form.username = username;
-        this.form.password = password;
-        this.form.role = role;
-        this.form.email = email;
-
-        this.dialogFormVisible = true;
-        this.dailogTitle = '编辑账号';
-      },
-      // 获取时间轴列表数据
-      GetHeartfeltList:function () {
-        var That = this;
-        That.SQAjax({
-          Url:'/api/userList',
-          Success:function (data) {
-            data.forEach(function (Item,I) {
-              Item.createdAt = Item.createdAt ? Item.createdAt.slice(0,10) : "";
-            });
-            That.userList = data;
-          }
-        });
-      },
-      // 删除账号
-      DeleteHeartfelt:function (name) {
-        var That = this;
-
-        That.SQAjax({
-          Url:'/api/deleteByUser',
-          RequestData:{
-            username:name
-          },
-          Success:function (data) {
+      if (this.form.id) {
+        // 修改
+        this.SQAjax({
+          Url: '/api/updateUser',
+          RequestData: this.form,
+          Success: function () {
             That.GetHeartfeltList();
+            That.CloseCreateDialog();
+          }
+        });
+      } else {
+        // 新增
+        this.SQAjax({
+          Url: '/api/register',
+          RequestData: this.form,
+          Success: function () {
+            That.GetHeartfeltList();
+            That.CloseCreateDialog();
           }
         });
       }
     },
-    mounted:function () {
-      this.GetHeartfeltList();
-      this.bus.$emit('Topbar',{
-        MenuHighLight:'9'
+    // 获取时间轴列表数据
+    GetHeartfeltList: function () {
+      var That = this;
+      That.SQAjax({
+        Url: '/api/userList',
+        Success: function (data) {
+          data.forEach(function (Item, I) {
+            Item.createdAt = Item.createdAt ? Item.createdAt.slice(0, 10) : "";
+          });
+          That.userList = data;
+        }
+      });
+    },
+    // 删除账号
+    DeleteHeartfelt: function (name) {
+      var That = this;
+
+      That.SQAjax({
+        Url: '/api/deleteByUser',
+        RequestData: {
+          username: name
+        },
+        Success: function (data) {
+          That.GetHeartfeltList();
+        }
       });
     }
+  },
+  mounted: function () {
+    this.GetHeartfeltList();
+    this.bus.$emit('Topbar', {
+      MenuHighLight: '9'
+    });
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
