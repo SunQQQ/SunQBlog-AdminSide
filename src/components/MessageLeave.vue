8b66cf11128ddfbd7cc2985b57d2a5e5560c68af
sunquan <1585437938@qq.com>
2025-02-12 13:32:06 +0800

    留言管理新增开发

--- src/components/MessageLeave.vue
+++ src/components/MessageLeave.vue
@@ -2,6 +2,10 @@
   <div>
     <div class="RightContent">
       <div class="ArticleList">
+        <div style="margin-bottom:10px">
+          <el-button type="primary" @click="OpenCreateDialog()" plain>创建留言</el-button>
+        </div>
+
         <el-dialog title="管理分类" :visible.sync="dialogFormVisible">
           <el-form :model="form">
             <el-form-item label="留言者" :label-width="formLabelWidth">
@@ -14,7 +18,10 @@
               <el-input v-model="form.city"></el-input>
             </el-form-item>
             <el-form-item label="头像编号(0-4)" :label-width="formLabelWidth">
-              <el-input v-model="form.avatar"></el-input>
+              <el-input v-model="form.avator"></el-input>
+            </el-form-item>
+            <el-form-item label="父级留言" :label-width="formLabelWidth">
+              <el-input v-model="form.parentId"></el-input>
             </el-form-item>
           </el-form>
           <div slot="footer" class="dialog-footer">
@@ -24,7 +31,7 @@
         </el-dialog>
 
         <!--表格操作栏-->
-        <el-table :data="MessageLeaveList" style="width: 100%" border :header-cell-style="{background:'#f7f7f7'}">
+        <el-table :data="MessageLeaveList" style="width: 100%" border :header-cell-style="{ background: '#f7f7f7' }">
           <el-table-column prop="leaveName" label="留言者"></el-table-column>
           <el-table-column prop="messageContent" label="留言内容">
             <template slot-scope="scope">
@@ -32,23 +39,20 @@
             </template>
           </el-table-column>
           <el-table-column prop="city" label="城市"></el-table-column>
-          <el-table-column prop="avatar" label="头像"></el-table-column>
+          <el-table-column prop="avator" label="头像"></el-table-column>
           <el-table-column prop="createTime" label="创建时间"></el-table-column>
           <el-table-column fixed="right" label="操作" width="130">
             <template slot-scope="scope">
-              <el-button @click="EditMessageLeave(scope.row)" type="text" size="small" class="warning-color">编辑</el-button>
+              <el-button @click="EditMessageLeave(scope.row)" type="text" size="small"
+                class="warning-color">编辑</el-button>
               <el-button @click="DeleteTag(scope.row._id)" type="text" size="small" class="danger-color">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
 
-        <div v-if="MessageLeaveTotal>10">
-          <el-pagination layout="prev, pager, next"
-                         :total=MessageLeaveTotal
-                         :page-size=PagiSize
-                         @current-change="ChangeCurPage"
-                         @next-click="NextPage"
-                         @prev-click="NextPage">
+        <div v-if="MessageLeaveTotal > 10">
+          <el-pagination layout="prev, pager, next" :total=MessageLeaveTotal :page-size=PagiSize
+            @current-change="ChangeCurPage" @next-click="NextPage" @prev-click="NextPage">
           </el-pagination>
         </div>
       </div>
@@ -57,135 +61,151 @@
 </template>
 
 <script>
-  export default {
-    name: "TagIndex",
-    data: function () {
-      return {
-        MessageLeaveList: [],
-        MessageLeaveTotal:0,
-        PagiSize:15,
-        dialogFormVisible: false,
-        form: {
-          leaveName: '',
-          messageContent: '',
-          delivery: false,
-          city: ''
-        },
-        formLabelWidth: '80px',
-        MyCurPage: 1
-      }
+export default {
+  name: "TagIndex",
+  data: function () {
+    return {
+      MessageLeaveList: [],
+      MessageLeaveTotal: 0,
+      PagiSize: 15,
+      dialogFormVisible: false,
+      form: {
+        leaveName: '',
+        messageContent: '',
+        delivery: false,
+        city: '',
+        avator: '',
+        parentId: '',
+        id: ''
+      },
+      formLabelWidth: '80px',
+      MyCurPage: 1
+    }
+  },
+  methods: {
+    // 打开新增弹框
+    OpenCreateDialog: function () {
+      this.form.id = '';
+      this.form.leaveName = '';
+      this.form.messageContent = '';
+      this.form.city = '';
+      this.form.avator = '';
+      this.form.parentId = '';
+
+      this.dialogFormVisible = true;
     },
-    methods: {
-      /*监听弹框提交*/
-      OnDialogSubmit:function(){
-        var That = this;
-        if(this.form.leaveName && this.form.messageContent){
-          this.SQAjax({
-            Url:'/api/MessageLeaveEdit/backend',
-            RequestData:this.form,
-            Success:function () {
-              That.SkipTo(That.MyCurPage);
-            }
-          });
-
-          this.dialogFormVisible = false;
-        }
-      },
-      OnDialogCancel:function(){
-        if(this.form.TagId){
-          delete this.form.TagId;
-        }
-        this.dialogFormVisible = false;
-      },
-      /*渲染标签列表*/
-      GetData:function () {
-        var That = this;
+    // 关闭新增弹框
+    CloseCreateDialog: function () {
+      this.dialogFormVisible = false;
+    },
+    /*监听弹框提交*/
+    OnDialogSubmit: function () {
+      var That = this;
+      if (this.form.leaveName && this.form.messageContent) {
         this.SQAjax({
-          // Url:'/api/MessageRead/foreend',
-          Url:'api/leaveMessageList',
-          RequestData: {
-            PagnationData: {
-              Skip:0,
-              Limit:16
-            }
-          },
-          Success:function (data) {
-            if (data.length > 10) {
-              data.pop();
-
-              That.SQAjax({
-                Url: '/api/getmessagenum',
-                Success: function (data) {
-                  That.MessageLeaveTotal = data;
-                }
-              });
-            }
-
-            data.forEach(function (Item,I) {
-              Item.createTime = Item.createTime.slice(0,10);
-            });
-
-            That.MessageLeaveList = data;
-          }
-        });
-      },
-      // 翻页方法
-      ChangeCurPage:function(CurPage){
-        this.SkipTo(CurPage);
-        this.MyCurPage = CurPage;
-      },
-      NextPage:function (CurPage) {
-        this.SkipTo(CurPage);
-        this.MyCurPage = CurPage;
-      },
-      SkipTo:function (CurPage) {
-        var That = this;
-        That.SQAjax({
-          Url:'/api/MessageRead/foreend',
-          RequestData: {
-            PagnationData: {
-              Skip:(CurPage-1) * 10,
-              Limit:10
-            }
-          },
-          Success:function (data) {
-            That.MessageLeaveList = data;
-          }
-        });
-      },
-      /*删除标签*/
-      DeleteTag:function (Id) {
-        var That = this;
-
-        That.SQAjax({
-          Url:'/api/MessageLeaveDelete/backend',
-          RequestData:{
-            _id:Id
-          },
-          Success:function () {
+          Url: '/api/createLeaveMessage',
+          RequestData: this.form,
+          Success: function () {
             That.SkipTo(That.MyCurPage);
           }
         });
-      },
-      /*编辑标签*/
-      EditMessageLeave:function (Row) {
-        this.form.leaveName = Row.leaveName;
-        this.form.messageContent = Row.messageContent;
-        this.form.city = Row.city;
-        this.form._id = Row._id;
-        this.form.avatar = Row.avatar;
-        this.dialogFormVisible = true;
-      },
+
+        this.dialogFormVisible = false;
+      }
     },
-    mounted: function () {
-      this.GetData();
-      this.bus.$emit('Topbar',{
-        MenuHighLight:'6'
+    OnDialogCancel: function () {
+      if (this.form.TagId) {
+        delete this.form.TagId;
+      }
+      this.dialogFormVisible = false;
+    },
+    /*渲染标签列表*/
+    GetData: function () {
+      var That = this;
+      this.SQAjax({
+        // Url:'/api/MessageRead/foreend',
+        Url: 'api/leaveMessageList',
+        RequestData: {
+          PagnationData: {
+            Skip: 0,
+            Limit: 16
+          }
+        },
+        Success: function (data) {
+          if (data.length > 10) {
+            data.pop();
+
+            That.SQAjax({
+              Url: '/api/getmessagenum',
+              Success: function (data) {
+                That.MessageLeaveTotal = data;
+              }
+            });
+          }
+
+          data.forEach(function (Item, I) {
+            Item.createTime = Item.createTime.slice(0, 10);
+          });
+
+          That.MessageLeaveList = data;
+        }
       });
-    }
+    },
+    // 翻页方法
+    ChangeCurPage: function (CurPage) {
+      this.SkipTo(CurPage);
+      this.MyCurPage = CurPage;
+    },
+    NextPage: function (CurPage) {
+      this.SkipTo(CurPage);
+      this.MyCurPage = CurPage;
+    },
+    SkipTo: function (CurPage) {
+      var That = this;
+      That.SQAjax({
+        Url: '/api/MessageRead/foreend',
+        RequestData: {
+          PagnationData: {
+            Skip: (CurPage - 1) * 10,
+            Limit: 10
+          }
+        },
+        Success: function (data) {
+          That.MessageLeaveList = data;
+        }
+      });
+    },
+    /*删除标签*/
+    DeleteTag: function (Id) {
+      var That = this;
+
+      That.SQAjax({
+        Url: '/api/MessageLeaveDelete/backend',
+        RequestData: {
+          _id: Id
+        },
+        Success: function () {
+          That.SkipTo(That.MyCurPage);
+        }
+      });
+    },
+    /*编辑标签*/
+    EditMessageLeave: function (Row) {
+      this.form.leaveName = Row.leaveName;
+      this.form.messageContent = Row.messageContent;
+      this.form.city = Row.city;
+      this.form._id = Row._id;
+      this.form.avator = Row.avator;
+      this.dialogFormVisible = true;
+    },
+  },
+  mounted: function () {
+    this.GetData();
+    this.bus.$emit('Topbar', {
+      MenuHighLight: '6'
+    });
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
